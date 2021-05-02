96c16a9350df3406f05d024b50c26a92771df433
LonelVino <lonelvino@gmail.com>
2021-05-02 09:16:51 +0200

    add Contest

---
+++ Java_code/SplitString/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            StringBuilder sb = new StringBuilder();
+            sb.append(str);
+            
+            int size = str.length();
+            int addZero = 8 - size%8;
+            while((addZero > 0) && (addZero < 8)) {
+                sb.append("0");
+                addZero--;
+            }
+            String str1 = sb.toString();
+            while(str1.length() > 0) {
+                System.out.println(str1.substring(0,8));
+                str1 = str1.substring(8);
+            }
+        }
+    }
+}
